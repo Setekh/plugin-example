78d5fff1d87e6fec9d8260d82a9c6868be55e6d5
Vlad Cazacu <vlad.ravenholm@gmail.com>
2021-03-31 11:30:51 +0300

    Add an example properties. This gets extracted on first install.

--- src/main/kotlin/com/example/ExampleEventListener.kt
+++ src/main/kotlin/com/example/ExampleEventListener.kt
@@ -6,12 +6,12 @@ import org.l2jmobius.gameserver.enums.ChatType
 import org.l2jmobius.gameserver.model.actor.instance.PlayerInstance
 import org.l2jmobius.gameserver.network.serverpackets.CreatureSay
 
-class ExampleEventListener: ArchidEx.Listener {
+class ExampleEventListener(val message: String): ArchidEx.Listener {
     override fun onEvent(key: Int, defaultValue: Any?, params: Array<out Any>): Any {
         when (key) {
             EventKeys.EnterWorld -> {
                 val player = params[0] as PlayerInstance
-                player.sendPacket(CreatureSay(null, ChatType.PARTYROOM_COMMANDER, "System", "Welcome back big boy!"))
+                player.sendPacket(CreatureSay(null, ChatType.PARTYROOM_COMMANDER, "System", message))
             }
         }
         return super.onEvent(key, defaultValue, params)
--- src/main/kotlin/com/example/ExamplePlugin.kt
+++ src/main/kotlin/com/example/ExamplePlugin.kt
@@ -3,15 +3,26 @@ package com.example
 import com.archid.ext.ArchidEx
 import com.archid.plugins.Plugin
 import com.archid.plugins.models.Manifest
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
 
 class ExamplePlugin: Plugin {
-    private val exampleListener = ExampleEventListener()
+    private var exampleListener: ExampleEventListener? = null
 
     override fun load(manifest: Manifest) {
+        val props = Properties()
+        props.load(FileInputStream(File(manifest.workDir, "example-settings.properties")))
+        val message = props.getProperty("WelcomeSystemMessage", "404: No message")
+
+        exampleListener = ExampleEventListener(message)
         ArchidEx.register(exampleListener)
+
+        props.clear()
     }
 
     override fun unload(manifest: Manifest) {
+        val exampleListener = exampleListener ?: return
         ArchidEx.unregister(exampleListener)
     }
 }
